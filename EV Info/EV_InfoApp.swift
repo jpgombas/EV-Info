4f90ff1cadc22730d1b9d10ef5e1795a5faa4e17
Jason <gombasja@msu.edu>
2025-08-29 09:09:54 -0400

    Initial Commit

---
+++ EV Info/EV_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EV_InfoApp.swift
+//  EV Info
+//
+//  Created by Jason on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct EV_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EV InfoTests/EV_InfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  EV_InfoTests.swift
+//  EV InfoTests
+//
+//  Created by Jason on 8/29/25.
+//
+
+import Testing
+@testable import EV_Info
+
+struct EV_InfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
